728b4e87eb1f020c5a547a43cd53db3f6de122f1
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2016-12-17 17:56:52 +0100

    Added debuggin for sums of powers

--- symbolic-calculator/debug_sycalc.pl
+++ symbolic-calculator/debug_sycalc.pl
@@ -1,4 +1,5 @@
 :-ensure_loaded(core).
+:-ensure_loaded(power_sums).
 
 debug:-
 	debug_integer_algs,
@@ -6,7 +7,8 @@ debug:-
 	debug_numbers,
 	debug_arithmetic_evaluation,
 	debug_monomials,
-	debug_polynomials.
+	debug_polynomials,
+	debug_power_sums.
 
 main:- debug, halt.
 main:- nl, write('ERROR'), nl, halt.
@@ -688,3 +690,118 @@ debug_polynomials:-
 	write(' - OK'), nl,
 	nl, true.
 
+/*
+-----------------------------
+DEBUG - POWER SUMS
+*/
+
+deb_power_sum_D(_, N, F, D):- expanded_polynomial_evaluation(N, F, RES), sum_from_1_to_n_to_D(N, D, RES), !.
+deb_power_sum_D(I, N, F, D):- expanded_polynomial_evaluation(N, F, RES), sum_from_1_to_n_to_D(N, D, R),
+	write(I), write(' Sum from 1 to '), write(N), write(' = '), output_text(R, RES).
+
+debug_power_sums:- 
+	write('-- POWER SUMS --'), nl,
+	write('* D = 1'),
+	optimized_power_sums(1, D1),
+	deb_power_sum_D(' 1.1)', 1, D1, 1),
+	deb_power_sum_D(' 1.2)', 5, D1, 1),
+	deb_power_sum_D(' 1.3)', 10, D1, 1),
+	deb_power_sum_D(' 1.4)', 20, D1, 1),
+	deb_power_sum_D(' 1.5)', 30, D1, 1),
+	deb_power_sum_D(' 1.6)', 50, D1, 1),
+	deb_power_sum_D(' 1.7)', 75, D1, 1),
+	deb_power_sum_D(' 1.8)', 100, D1, 1),
+
+	write(' - OK'), nl,
+	write('* D = 2'),
+
+	optimized_power_sums(2, D2),
+	deb_power_sum_D(' 2.1)', 1, D2, 2),
+	deb_power_sum_D(' 2.2)', 5, D2, 2),
+	deb_power_sum_D(' 2.3)', 10, D2, 2),
+	deb_power_sum_D(' 2.4)', 20, D2, 2),
+	deb_power_sum_D(' 2.5)', 30, D2, 2),
+	deb_power_sum_D(' 2.6)', 50, D2, 2),
+	deb_power_sum_D(' 2.7)', 75, D2, 2),
+	deb_power_sum_D(' 2.8)', 100, D2, 2),
+
+	write(' - OK'), nl,
+	write('* D = 3'),
+
+	optimized_power_sums(3, D3),
+	deb_power_sum_D(' 3.1)', 1, D3, 3),
+	deb_power_sum_D(' 3.2)', 5, D3, 3),
+	deb_power_sum_D(' 3.3)', 10, D3, 3),
+	deb_power_sum_D(' 3.4)', 20, D3, 3),
+	deb_power_sum_D(' 3.5)', 30, D3, 3),
+	deb_power_sum_D(' 3.6)', 50, D3, 3),
+	deb_power_sum_D(' 3.7)', 75, D3, 3),
+	deb_power_sum_D(' 3.8)', 100, D3, 3),
+
+	write(' - OK'), nl,
+	write('* D = 5'),
+
+	optimized_power_sums(5, D5),
+	deb_power_sum_D(' 5.1)', 1, D5, 5),
+	deb_power_sum_D(' 5.2)', 5, D5, 5),
+	deb_power_sum_D(' 5.3)', 10, D5, 5),
+	deb_power_sum_D(' 5.4)', 20, D5, 5),
+	deb_power_sum_D(' 5.5)', 30, D5, 5),
+	deb_power_sum_D(' 5.6)', 50, D5, 5),
+	deb_power_sum_D(' 5.7)', 75, D5, 5),
+	deb_power_sum_D(' 5.8)', 100, D5, 5),
+
+	write(' - OK'), nl,
+	write('* D = 7'),
+
+	optimized_power_sums(7, D7),
+	deb_power_sum_D(' 7.1)', 1, D7, 7),
+	deb_power_sum_D(' 7.2)', 5, D7, 7),
+	deb_power_sum_D(' 7.3)', 10, D7, 7),
+	deb_power_sum_D(' 7.4)', 20, D7, 7),
+	deb_power_sum_D(' 7.5)', 30, D7, 7),
+	deb_power_sum_D(' 7.6)', 50, D7, 7),
+	deb_power_sum_D(' 7.7)', 75, D7, 7),
+	deb_power_sum_D(' 7.8)', 100, D7, 7),
+
+	write(' - OK'), nl,
+	write('* D = 10'),
+
+	optimized_power_sums(10, D10),
+	deb_power_sum_D(' 10.1)', 1, D10, 10),
+	deb_power_sum_D(' 10.2)', 5, D10, 10),
+	deb_power_sum_D(' 10.3)', 10, D10, 10),
+	deb_power_sum_D(' 10.4)', 20, D10, 10),
+	deb_power_sum_D(' 10.5)', 30, D10, 10),
+	deb_power_sum_D(' 10.6)', 50, D10, 10),
+	deb_power_sum_D(' 10.7)', 75, D10, 10),
+	deb_power_sum_D(' 10.8)', 100, D10, 10),
+
+	write(' - OK'), nl,
+	write('* D = 15'),
+
+	optimized_power_sums(15, D15),
+	deb_power_sum_D(' 15.1)', 1, D15, 15),
+	deb_power_sum_D(' 15.2)', 5, D15, 15),
+	deb_power_sum_D(' 15.3)', 10, D15, 15),
+	deb_power_sum_D(' 15.4)', 20, D15, 15),
+	deb_power_sum_D(' 15.5)', 30, D15, 15),
+	deb_power_sum_D(' 15.6)', 50, D15, 15),
+	deb_power_sum_D(' 15.7)', 75, D15, 15),
+	deb_power_sum_D(' 15.8)', 100, D15, 15),
+
+	write(' - OK'), nl,
+	write('* D = 20'),
+
+	optimized_power_sums(20, D20),
+	deb_power_sum_D(' 20.1)', 1, D20, 20),
+	deb_power_sum_D(' 20.2)', 5, D20, 20),
+	deb_power_sum_D(' 20.3)', 10, D20, 20),
+	deb_power_sum_D(' 20.4)', 20, D20, 20),
+	deb_power_sum_D(' 20.5)', 30, D20, 20),
+	deb_power_sum_D(' 20.6)', 50, D20, 20),
+	deb_power_sum_D(' 20.7)', 75, D20, 20),
+	deb_power_sum_D(' 20.8)', 100, D20, 20),
+
+	write(' - OK'), nl,
+	nl, true.
--- symbolic-calculator/power_sums.pl
+++ symbolic-calculator/power_sums.pl
@@ -1,78 +1,40 @@
 :-ensure_loaded(core).
 
+% FORMULA OF SUMS OF POWERS
+
 h_coefficient(C, H):- arithmetic_eval(C + 1, R), fraction_comp(R, N, D), fraction_comp(H, D, N).
 
-%%%%%%%%%%%%%%%%%%
-% power_sums 1
+reminder_(_, [], 0):- !.
+reminder_([[D, F]|_], [M], R):-
+	monomial_degree(M, D), monomial_coefficient(M, MC), polynomial_evaluation(MC*F, R), !.
+reminder_([_|Fs], Ms, R):- reminder_(Fs, Ms, R), !.
+reminder_([[D, F]|Fs], [M|Ms], R):-
+	monomial_degree(M, D), monomial_coefficient(M, MC), polynomial_evaluation(MC*F, R1),
+	reminder_(Fs, Ms, R2), polynomial_evaluation(R1 + R2, R), !.
+reminder_([_|Fs], Ms, R):- reminder_(Fs, Ms, R).
 
-reminder_([M], R):- monomial_degree(M, MD), power_sums(MD, S), monomial_coefficient(M, MC), polynomial_evaluation(MC*S, R), !.
-reminder_([M|MS], R):- reminder_([M], R1), reminder_(MS, R2), polynomial_evaluation(R1 + R2, R), !.
-reminder(P, R):- polynomial_monomials(P, MS), reminder_(MS, R).
+reminder(F, P, R):- polynomial_monomials(P, MS), reminder_(F, MS, R).
 
-power_sums(1, (1/2)*n^2 + (1/2)*n):- !.
-power_sums(D, S):- D1 is D - 1, power_sums(D1, S1),			% sum of (D - 1)-powers
-	polynomial_evaluation((n + 1)*S1, SKY),					% write('SKY= '), write(SKY), nl,
-	polynomial_first_monomial(S1, F, R), 					% write('    First monomial= '), write(F), nl,
-															% write('    Other monomials= '), write(R), nl,
-	monomial_coefficient(F, HH), 							% write('    Monomial coefficient= '), write(HH), nl,
-	h_coefficient(HH, HUMANS), 								% write('    Humans= '), write(HUMANS), nl,
-	reminder(R, REMINDER),									% write('    Reminder= '), write(REMINDER), nl,
-
-	polynomial_evaluation( HUMANS*(SKY - REMINDER), S ),
-	!.
-
-%%%%%%%%%%%%%%%%%%
-% power_sums 2
-
-reminder2_(_, [], 0):- !.
-reminder2_([[D, F]|_], [M], R):-
-	%write('reminder2 (1): '), nl,
-	%write('    [F]='), write([[D, F]|Fs]), write(' [M]='), write([M]), nl,
-	%write('    F='), write([D, F]), write(' M='), write(M), nl,
-	monomial_degree(M, D),
-	monomial_coefficient(M, MC), polynomial_evaluation(MC*F, R),
-	%write('    correct here'), nl,
-	!.
-reminder2_([_|Fs], Ms, R):-
-	%write('reminder2 (3): '), nl,
-	%write('    F='), write(Fs), write(' M='), write(Ms), nl,
-	reminder2_(Fs, Ms, R),
-	!.
-reminder2_([[D, F]|Fs], [M|Ms], R):-
-	%write('reminder2 (2): '), nl,
-	%write('    [F]='), write([[D, F]|Fs]), write(' [M]='), write([M|Ms]), nl,
-	%write('    F='), write([D, F]), write(' M='), write(M), nl,
-	monomial_degree(M, D),
-	monomial_coefficient(M, MC), polynomial_evaluation(MC*F, R1),
-	reminder2_(Fs, Ms, R2), polynomial_evaluation(R1 + R2, R),
-	%write('    correct here'), nl,
-	!.
-reminder2_([_|Fs], Ms, R):-
-	%write('reminder2 (3): '), nl,
-	%write('    F='), write(Fs), write(' M='), write(Ms), nl,
-	reminder2_(Fs, Ms, R).
-
-reminder2(F, P, R):- polynomial_monomials(P, MS), reminder2_(F, MS, R).
-
-optimized_power_sums_(1, (1/2)*n^2 + (1/2)*n, [[1, (1/2)*n^2 + (1/2)*n]]):- !.
-optimized_power_sums_(D, S, L):- 
-	D1 is D - 1, optimized_power_sums_(D1, S1, L1),			% sum of (D - 1)-powers
-															%write('D='), write(D1), nl,
-	polynomial_evaluation((n + 1)*S1, SKY),					%write('    sky= '), write(SKY), nl,
-															%write('    L1='), write(L1), nl,
-	polynomial_first_monomial(S1, F, R), 					%write('    First monomial= '), write(F), nl,
-															%write('    Pre reminder= '), write(R), nl,
-	monomial_coefficient(F, HH), 							%write('    Monomial coefficient= '), write(HH), nl,
-	h_coefficient(HH, HUMANS), 								%write('    Humans= '), write(HUMANS), nl,
-	reminder2(L1, R, REMINDER),								%write('    Reminder= '), write(REMINDER), nl,
-
-	polynomial_evaluation(HUMANS*(SKY - REMINDER), S),		%write('        Formula='), write(S), nl,
+power_sums_(1, (1/2)*n^2 + (1/2)*n, [[1, (1/2)*n^2 + (1/2)*n]]):- !.
+power_sums_(D, S, L):- 
+	D1 is D - 1, power_sums_(D1, S1, L1),
+	polynomial_evaluation((n + 1)*S1, SKY),
+	polynomial_first_monomial(S1, F, R),
+	monomial_coefficient(F, HH),
+	h_coefficient(HH, HUMANS),
+	reminder(L1, R, REMINDER),
+	polynomial_evaluation(HUMANS*(SKY - REMINDER), S),
 	concat([[D,S]], L1, L),
 	!.
 
-optimized_power_sums(D, S):- optimized_power_sums_(D, S, _).
+% S = f(n) = 1^D + 2^D + ... + n^D
+power_sums(D, S):- power_sums_(D, S, _).
 
+% L = [i, f(n, i)] where f(n, i) = 1^i + 2^i + ... + n^i
+power_sums_list(D, L):- power_sums_(D, _, L).
 
-% power_sums(1, S1), power_sums(2, S2), power_sums(3, S3), power_sums(4, S4),
-% polynomial_evaluation((5/6)*((n + 1)*S4 - 1/2*S4 - 1/3*S3 + 1/30*S1), S5),
-% power_sums(5, S5),
+% Computes iteratively the sum: 1^D + 2^D + ... + N^D
+sum_from_1_to_n_to_D(1, _, 1):- !.
+sum_from_1_to_n_to_D(N, D, S):-
+	N1 is N - 1, sum_from_1_to_n_to_D(N1, D, S1),
+	S is S1 + N^D.
