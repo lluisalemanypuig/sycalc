58613c667c568b435f7c07df717511f34e8a4f60
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2017-04-22 20:33:39 +0200

    Nice display of use of polynomials for sums of powers computation

--- c++/src/main.cpp
+++ c++/src/main.cpp
@@ -6,9 +6,44 @@
 using namespace std;
 
 /// Custom includes
+#include "algorithms.hpp"
+#include "polynomial.hpp"
 #include "tests.hpp"
+#include "timing.hpp"
+
+using namespace sycalc;
+using namespace mtime;
+using namespace algorithms;
+using namespace core;
 
 int main(int argc, char *argv[]) {
-	sycalc::tests::algorithms_tests();
+	//sycalc::tests::algorithms_tests();
+	
+	for (size_t d = 10; d <= 20; ++d) {
+		cout << "d -> " << d << endl;
+		
+		polynomial p;
+		const size_t inf_lim = 2500001;
+		const size_t sup_lim = 2500010;
+		
+		timing begin = now();
+		algorithms::power_sums(d, p);
+		cout << "    p(n)= " << p << endl;
+		for (size_t n = inf_lim; n <= sup_lim; ++n) {
+			cout << "        p(" << n << ")= " << p.evaluate(n) << endl;
+		}
+		timing end = now();
+		cout << "        * Polynomial computation and evaluation: " << elapsed_time(begin, end) << " s" << endl;
+		
+		begin = now();
+		cout << "        ---" << endl;
+		for (size_t n = inf_lim; n <= sup_lim; ++n) {
+			cout << "        L(" << n << ")= " << algorithms::linear_power_sums(1, n, d) << endl;
+		}
+		end = now();
+		cout << "        * Linear computation: " << elapsed_time(begin, end) << " s" << endl;
+		
+		cout << endl;
+	}
 }
 
