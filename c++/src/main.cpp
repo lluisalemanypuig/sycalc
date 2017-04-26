42918eed417dcb2c6ffdd85b5842811b6c760066
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2017-04-26 17:12:03 +0200

    Better main

--- c++/src/main.cpp
+++ c++/src/main.cpp
@@ -17,9 +17,12 @@ using namespace algorithms;
 using namespace core;
 
 int main(int argc, char *argv[]) {
+	//sycalc::tests::polynomial_tests();
 	//sycalc::tests::algorithms_tests();
 	
-	for (size_t d = 10; d <= 20; ++d) {
+	timing gbegin = now();
+	const size_t s = 100;
+	for (size_t d = s; d <= s + 20; ++d) {
 		cout << "d -> " << d << endl;
 		
 		polynomial p;
@@ -28,13 +31,14 @@ int main(int argc, char *argv[]) {
 		
 		timing begin = now();
 		algorithms::power_sums(d, p);
-		cout << "    p(n)= " << p << endl;
+		/*cout << "    p(n)= " << p << endl;
 		for (size_t n = inf_lim; n <= sup_lim; ++n) {
 			cout << "        p(" << n << ")= " << p.evaluate(n) << endl;
-		}
+		}*/
 		timing end = now();
 		cout << "        * Polynomial computation and evaluation: " << elapsed_time(begin, end) << " s" << endl;
 		
+		/*
 		begin = now();
 		cout << "        ---" << endl;
 		for (size_t n = inf_lim; n <= sup_lim; ++n) {
@@ -44,6 +48,9 @@ int main(int argc, char *argv[]) {
 		cout << "        * Linear computation: " << elapsed_time(begin, end) << " s" << endl;
 		
 		cout << endl;
+		*/
 	}
+	timing gend = now();
+	cout << "    * Total time: " << elapsed_time(gbegin, gend) << " s" << endl;
 }
 
