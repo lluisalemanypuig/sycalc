22319921452f4ec3377c24f29b04091670a7c071
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-04-07 00:16:16 +0200

    Improved monomial addition and substraction
    The result of two monomials without common variable is the two monomials
    sorted so that the monomial with the first variable (according to @<) is
    placed at the beginning of the resulting expression.

--- src/core/monomial_evaluation.pl
+++ src/core/monomial_evaluation.pl
@@ -1,78 +1,123 @@
 :-ensure_loaded(monomial).
 
-%			   C1, V1, E1, C2, V2, E2, R
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% TO BE USED ONLY WHEN V1 \= V2 %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-pretty_monomials_prod_comp( 0, _,  _,  _, _,  _, 0).
-pretty_monomials_prod_comp( _, _,  _,  0, _,  _, 0).
-pretty_monomials_prod_comp( 1, _,  0,  1, _,  0, 1).
-pretty_monomials_prod_comp( 1, _,  0,  1, Y,  1, Y).
-pretty_monomials_prod_comp( 1, X,  1,  1, _,  0, X).
-pretty_monomials_prod_comp( 1, X,  1,  1, Y,  1, X*Y).
-pretty_monomials_prod_comp(CX, _,  0, CY, _,  0, C):- arithmetic_eval(CX*CY, C), !.
-pretty_monomials_prod_comp(CX, _,  0, CY, Y,  1, C*Y):- arithmetic_eval(CX*CY, C), !.
-pretty_monomials_prod_comp(CX, X,  1, CY, _,  0, C*X):- arithmetic_eval(CX*CY, C), !.
-pretty_monomials_prod_comp(CX, X,  1, CY, Y,  1, C*X*Y):- arithmetic_eval(CX*CY, C), !.
-pretty_monomials_prod_comp(CX, X,  1, CY, Y, EY, C*X*(Y^EY)):- arithmetic_eval(CX*CY, C), !.
-pretty_monomials_prod_comp(CX, X, EX, CY, Y,  1, C*(X^EX)*Y):- arithmetic_eval(CX*CY, C), !.
-pretty_monomials_prod_comp(CX, X, EX, CY, Y, EY, C*(X^EX)*(Y^EY)):- arithmetic_eval(CX*CY, C).
+% multiplies two monomials assuming their variables are different.
+% the result is a multivariate monomial (this is the only predicate to
+% produce this kind of monomials - the rest are always univariate).
+%		   C1, V1, E1, C2, V2, E2, R
+pretty_monomials_prod_comp( 0,  _,  _,  _,  _,  _, 0).
+pretty_monomials_prod_comp( _,  _,  _,  0,  _,  _, 0).
+pretty_monomials_prod_comp( 1,  _,  0,  1,  _,  0, 1).
+pretty_monomials_prod_comp( 1,  _,  0,  1,  Y,  1, Y).
+pretty_monomials_prod_comp( 1,  X,  1,  1,  _,  0, X).
+pretty_monomials_prod_comp( 1,  X,  1,  1,  Y,  1, X*Y).
+pretty_monomials_prod_comp(CX,  _,  0, CY,  _,  0, C):- arithmetic_eval(CX*CY, C), !.
+pretty_monomials_prod_comp(CX,  _,  0, CY,  Y,  1, C*Y):- arithmetic_eval(CX*CY, C), !.
+pretty_monomials_prod_comp(CX,  X,  1, CY,  _,  0, C*X):- arithmetic_eval(CX*CY, C), !.
+pretty_monomials_prod_comp(CX,  X,  1, CY,  Y,  1, C*X*Y):- arithmetic_eval(CX*CY, C), !.
+pretty_monomials_prod_comp(CX,  X,  1, CY,  Y, EY, C*X*(Y^EY)):- arithmetic_eval(CX*CY, C), !.
+pretty_monomials_prod_comp(CX,  X, EX, CY,  Y,  1, C*(X^EX)*Y):- arithmetic_eval(CX*CY, C), !.
+pretty_monomials_prod_comp(CX,  X, EX, CY,  Y, EY, C*(X^EX)*(Y^EY)):- X @< Y, arithmetic_eval(CX*CY, C).
+pretty_monomials_prod_comp(CX,  X, EX, CY,  Y, EY, C*(Y^EY)*(X^EX)):- arithmetic_eval(CX*CY, C).
 
 % -----------
 % ARITHMETIC OPERATIONS WITH MONOMIALS
 
-% ADDITION
+%% ADDITION
+
+% sorted addition of two monomials that cannot be 'fused' into a single
+% one:
+% if the variables are different, sort by variable
+% if the variables are equal but exponents are different, sort by
+% exponent
+% if the variables and the exponents are equal the result is a single
+% monomial -> this case does not apply
+
+% null coeficients
+mon_sum_( _, 0, _, _,   _, 0, _, _, 0):- !.
+mon_sum_( _, 0, _, _,   _, C, V, E, M):- red_monomial_comps(C,V,E, M), !.
+mon_sum_( _, C, V, E,   _, 0, _, _, M):- red_monomial_comps(C,V,E, M), !.
+% null exponents
+mon_sum_( _,C1, _, 0,   _,C2, _, 0, R):- arithmetic_eval(C1 + C2,R), !.
+mon_sum_( M, _, _, _,   _,C2, _, 0, M + C2):- !.
+mon_sum_( _,C1, _, 0,   M, _, _, _, C1 + M):- !.
+% same variables, same exponents
+mon_sum_( _,C1,V1,E1,   _,C2,V1,E1,  R):- red_monomial_comps(C1 + C2, V1, E1, R), !.
+% same variables, different exponents
+mon_sum_(M1, _,V1,E1,  M2,C2,V1,E2,  M1 + M2):- E1 > E2, C2 > 0, !.
+mon_sum_(M1, _,V1,E1,   _,C2,V1,E2,  M1 - N2):-
+	E1 > E2, C2 < 0,
+	rational_neg(C2,K), red_monomial_comps(K,V1,E2, N2), !.
+mon_sum_(M1,C1,V1,E1,  M2, _,V1,E2,  M2 + M1):- E1 < E2, C1 > 0, !.
+mon_sum_( _,C1,V1,E1,  M2, _,V1,E2,  M2 - N1):-
+	E1 < E2, C1 < 0,
+	rational_neg(C1,K), red_monomial_comps(K,V1,E1, N1), !.
+% different variables
+mon_sum_(M1, _,V1, _,  M2,C2,V2, _, M1 + M2):- C2 > 0, V1 @< V2, !.
+mon_sum_(M1, _,V1, _,  M2,C2,V2, _, M2 + M1):- C2 > 0, V2 @< V1, !.
+mon_sum_(M1, _, _, _,   _,C2,V2,E2, M1	- N2):-
+	rational_neg(C2, K), red_monomial_comps(K,V2,E2, N2), !.
 
 mon_sum(0, M2, M3):- red_monomial(M2, M3), !.
 mon_sum(M1, 0, M3):- red_monomial(M1, M3), !.
-mon_sum(M1, -(M2), R):- mon_sub(M1, M2, R), !.
-mon_sum(M1, M2, M3 + M4):-
-	monomial_comps(M1, _, V1, _), monomial_comps(M2, _, V2, _),
-	V1 \= V2,
-	red_monomial(M1, M3), red_monomial(M2, M4), !.
-
-mon_sum(M1, M2, M3):-
-	monomial_comps(M1, C1, V1, E1), monomial_comps(M2, C2, _, E2),
-	E1 == E2,
-	arithmetic_eval(C1 + C2, S), !, red_monomial_comps(S, V1, E1, M3).
-
 mon_sum(M1, M2, R):-
-	red_monomial(M1, M3), red_monomial(M2, M4),
-	monomial_comps(M3, _, V, E3), monomial_comps(M4, _, V, E4),
-	E3 == E4,
-	mon_sum(M3, M4, R), !.
-
-mon_sum(M1, M2, M3 + M4):-
-	monomial_comps(M1, C1, V, E1), monomial_comps(M2, C2, V, E2),
-	red_monomial_comps(C1, V, E1, M3), red_monomial_comps(C2, V, E2, M4), !.
+	monomial_comps(M1, C1, V1, E1),
+	monomial_comps(M2, C2, V2, E2),
+	mon_sum_(M1,C1,V1,E1,  M2,C2,V2,E2,  R).
 
 mon_sum([M1,M2], S):- mon_sum(M1, M2, S), !.
 
-% SUBSTRACTION
+%% SUBSTRACTION
 
-mon_sub(0, M2, M3):-
-	monomial_comps(M2, C, V, E), rational_neg(C, K),
-	red_monomial_comps(K, V, E, M3), !.
+% sorted substraction of two monomials that cannot be 'fused' into a
+% single one:
+% if the variables are different, sort by variable
+% if the variables are equal but exponents are different, sort by
+% exponent
+% if the variables and the exponents are equal the result is a
+% single monomial -> this case does not apply
+
+% null coeficients
+mon_sub_( _, 0, _, _,   _, 0, _, _, 0):- !.
+mon_sub_( _, 0, _, _,   _, C, V, E, M):- rational_neg(C,K), red_monomial_comps(K,V,E, M), !.
+mon_sub_( _, C, V, E,   _, 0, _, _, M):- red_monomial_comps(C,V,E, M), !.
+% null exponents
+mon_sub_( _,C1, _, 0,   _,C2, _, 0, R):- arithmetic_eval(C1 - C2,R), !.
+mon_sub_( M, _, _, _,   _,C2, _, 0, M - C2):- !.
+mon_sub_( _,C1, _, 0,   M,C2, _, _, C1 - M):- C2 > 0, !.
+mon_sub_( _,C1, _, 0,   _,C2,V2,E2, C1 + N):-
+	C2 < 0, rational_neg(C2,K), red_monomial_comps(K,V2,E2, N), !.
+% same variables, same exponents
+mon_sub_( _,C1,V1,E1,   _,C2,V1,E1,  R):- red_monomial_comps(C1 - C2, V1, E1, R), !.
+% same variables, different exponents
+mon_sub_(M1, _,V1,E1,  M2,C2,V1,E2,  M1 - M2):- E1 > E2, C2 > 0, !.
+mon_sub_(M1, _,V1,E1,   _,C2,V1,E2,  M1 - N2):-
+	E1 > E2, C2 < 0,
+	rational_neg(C2,K), red_monomial_comps(K,V1,E2, N2), !.
+mon_sub_(M1,C1,V1,E1,  M2, _,V1,E2, M2 + M1):- E1 < E2, C1 > 0, !.
+mon_sub_( _,C1,V1,E1,  M2, _,V1,E2, M2 - N1):-
+	E1 < E2, C1 < 0,
+	rational_neg(C1,K), red_monomial_comps(K,V1,E1, N1), !.
+% different variables
+mon_sub_(M1, _,V1, _,  M2,C2,V2, _, M1 - M2):-
+	V1 @< V2, C2 > 0, !.
+mon_sub_(M1, _,V1, _,   _,C2,V2,E2, M1 + N1):-
+	V1 @< V2, C2 < 0,
+	rational_neg(C2,K), red_monomial_comps(K,V2,E2, N1), !.
+
+mon_sub_(M1,C1,V1, _,   _,C2,V2,E2, N2 + M1):-
+	V1 @> V2, C1 > 0,
+	rational_neg(C2,K), red_monomial_comps(K,V2,E2, N2), !.
+mon_sub_( _,C1,V1,E1,   _,C2,V2,E2, N2  - N1):-
+	V1 @> V2, C1 < 0,
+	rational_neg(C1,K1), red_monomial_comps(K1,V1,E1, N1),
+	rational_neg(C2,K2), red_monomial_comps(K2,V2,E2, N2), !.
+
+mon_sub(0, M2, M3):- red_monomial(M2, M3), !.
 mon_sub(M1, 0, M3):- red_monomial(M1, M3), !.
-mon_sub(M1, M2, M3 - M4):-
-	monomial_comps(M1, _, V1, _), monomial_comps(M2, _, V2, _),
-	V1 \= V2,
-	red_monomial(M1, M3), red_monomial(M2, M4), !.
-
-mon_sub(M1, M2, M3):-
-	monomial_comps(M1, C1, V1, E1), monomial_comps(M2, C2, _, E2),
-	E1 == E2,
-	arithmetic_eval(C1 - C2, S), !, red_monomial_comps(S, V1, E1, M3).
-
 mon_sub(M1, M2, R):-
-	red_monomial(M1, M3), red_monomial(M2, M4),
-	monomial_comps(M3, _, V, E3), monomial_comps(M4, _, V, E4),
-	E3 == E4,
-	mon_sub(M3, M4, R), !.
-
-mon_sub(M1, M2, M3 - M4):-
-	monomial_comps(M1, C1, V, E1), monomial_comps(M2, C2, V, E2),
-	red_monomial_comps(C1, V, E1, M3), red_monomial_comps(C2, V, E2, M4), !.
+	monomial_comps(M1, C1, V1, E1),
+	monomial_comps(M2, C2, V2, E2),
+	mon_sub_(M1,C1,V1,E1,  M2,C2,V2,E2,  R).
 
 mon_sub([M1,M2], S):- mon_sub(M1, M2, S), !.
 
