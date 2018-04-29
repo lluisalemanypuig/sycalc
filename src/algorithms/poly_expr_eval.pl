15c41dadd0fb77daf253e12e9754f1ef38224b53
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-04-29 18:28:34 +0200

    Added monomial_symb_evaluation predicate
    For symbolic evaluation of monomials

--- src/algorithms/poly_expr_eval.pl
+++ src/algorithms/poly_expr_eval.pl
@@ -1,5 +1,41 @@
 :-ensure_loaded("../core").
 
+% MONOMIAL SYMBOLIC EVALUATION
+
+% The symbolic evaluation of monomial M at variable V with expression E
+% is the resulting symbolic expression of replacing variable V inside
+% monomial M with E.
+monomial_symb_evaluation(V, E, M, N):-
+	monomial_split(V, M, Vo,Wo),
+	monomial_var_exp(Vo, V, V^P),
+	polynomial_evaluation( Wo*(E^P), N ).
+
+% MONOMIAL SUMMATION EVALUATION
+
+% The summation of a monomial M over variable v_k from I to F (1) is
+% the product of the coefficient and the variables different from v_k of
+% the monomial and the difference between the summation of the monomial
+% v_i^p_i over variable v_k from 0 to F and the summation of the
+% monomial v_i^p_i over variable v_k from 0 to I - 1 (2).
+% (1)
+%     sum_{v_k=I}^{F} M
+%     where M = coefficient * {v_i^p_i},
+%         v_i is a variable
+%         p_i is a rational value
+% (1.1)
+%     if v_k is the k-th variable then M can be denoted as
+%         M = c*v_1^p_1*...*v_k^p_k*...* v_n^p_n
+% (1.2)
+%     M without v_k is
+%  M(-v_k) = c*v_1^p_1*...*v_{k-1}^p_{k-1}*v_{k+1}^p_{k+1}*...*v_n^p_n
+% (1.3)
+%     If a monomial does not have variable v_k then
+%     M(-v_k) = M
+% (2)
+%     sum_{v_k=I}^{F} M =
+%     M(-v_k)*(sum_{v_k=0}^{F} (v_k^p_k) - sum_{v_k=0}^{I-1} (v_k^p_k))
+monomial_summation(VAR, I,F, M, S).
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% POLYNOMIAL EXPRESSIONS' EVALUATION %%%%
 
