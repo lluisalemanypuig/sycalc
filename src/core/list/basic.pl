4d2997a1ea823f27029708e81fc6290e93e419e7
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-07-18 17:12:58 +0200

    Trying to conform the documentation with the format allowed in 'dyp'

--- src/core/list/basic.pl
+++ src/core/list/basic.pl
@@ -6,9 +6,9 @@
 
 /**
 	@form min(List, Min)
-	@constrs List must have at least one element (what is the minimum
-	value of a list with no values?).
-	@descr Min is the smallest value, according to '@>' in List
+	@descr @Min is the smallest value, according to '@>', in @List.
+	@constrs
+		@param List Must have at least one element.
 */
 min([X], X):- !.
 min([X|L], M):- min(L, N), X @> N, !, M is N.
@@ -16,9 +16,9 @@ min([X|_], X).
 
 /**
 	@form max(List, Max)
-	@constrs List must have at least one element (what is the maximum
-	value of a list with no values?).
-	@descr Max is the smallest value, according to '@<' in List
+	@descr @Max is the smallest value, according to '@<', in @List.
+	@constrs
+		@param List Must have at least one element.
 */
 max([X], X):- !.
 max([X|L], M):- max(L, N), X @< N, !, M is N.
@@ -26,44 +26,48 @@ max([X|_], X).
 
 /**
 	@form first(List, First, Rest)
-	@constrs List cannot be empty.
-	@descr First is the head of List and Rest are the other elements of
-	List.
+	@descr @First is the head of @List and @Rest are the other elements
+	of @List.
+	@constrs
+		@param List Must have at least one element.
 */
 first([X], X, []):- !.
 first([X|L], X, L).
 
 /**
 	@form last(List, Rest, Last)
-	@constrs List cannot be empty.
-	@descr Last is the last element of List. Rest are the elements from
-	the first to the second to last.
+	@descr @Last is the last element of @List. @Rest are the elements
+	from the first to the second to last.
+	@constrs
+		@param List Must have at least one element.
 */
 last([X], [], X):- !.
 last([X|R], [X|K], L):- last(R, K, L).
 
 /**
 	@form replace_first(List, Value, NewList)
-	@constrs List cannot be empty.
-	@descr NewList is List but the first value is now Value. NewList
-	has the same length as List.
+	@descr @NewList is @List but its first value is now @Value. @NewList
+	has the same length as @List.
+	@constrs
+		@param List Must have at least one element.
 */
 replace_first([_], Y, [Y]):- !.
 replace_first([_|L], Y, [Y|L]).
 
 /**
 	@form replace_last(List, Value, NewList)
-	@constrs List cannot be empty.
-	@descr NewList is List but the last value is now Value. NewList
-	has the same length as List.
+	@descr @NewList is @List but the last value is now @Value. @NewList
+	has the same length as @List.
+	@constrs
+		@param List Must have at least one element.
 */
 replace_last([_], Y, [Y]):- !.
 replace_last([X|L], Y, [X|R]):- replace_last(L, Y, R).
 
 /**
 	@form replace(Value,With, List, NewList)
-	@descr This predicete replaces all elements equal to Value with
-	value With in List. NewList is the result of this substitution.
+	@descr This predicete replaces all elements equal to @Value with
+	value @With in @List. @NewList is the result of this substitution.
 */
 replace(_, _,     [],     []):- !.
 replace(V, I,    [V],    [I]):- !.
@@ -73,8 +77,8 @@ replace(V, I, [X|Xs], [X|Ds]):- replace(V, I, Xs, Ds).
 
 /**
 	@form drop(Value, List, NewList)
-	@descr NewList contains all elements from List except for those
-	equal to Value, according to \=.
+	@descr @NewList contains all elements from @List except for those
+	equal to @Value, according to '\='.
 */
 drop(_,    [],      []):- !.
 drop(O,    [O],     []):- !.
@@ -84,11 +88,11 @@ drop(O, [X|Xs], [X|Ds]):- drop(O, Xs, Ds).
 
 /**
 	@form pdrop(Value, List1,List2, NewList1,NewList2)
-	@constrs List1 and List2 must have the same length.
-	@descr NewList1 contains all elements from List1 except for those
-	equal to Value, according to \=. If i-th element of List1 is
-	dropped then the i-th element of List2 is also dropped and NewList2
-	will not contain it.
+	@descr @NewList1 contains all elements from @List1 except for those
+	equal to @Value, according to '\='. If i-th element of @List1 is
+	dropped then the i-th element of @List2 is also dropped and
+	@NewList2 will not contain it.
+	@constrs @List1 and @List2 must have the same length.
 */
 pdrop(_,         [],[],           [],[]):- !.
 pdrop(X,       [X],[_],           [],[]):- !.
@@ -98,11 +102,11 @@ pdrop(X, [Y|Xs],[Q|Qs], [Y|Dxs],[Q|Dqs]):- pdrop(X, Xs,Qs, Dxs,Dqs).
 
 /**
 	@form split(Value,List, Left,Right)
-	@constrs List can not be empty.
-	@descr Splits List into two groups, Left and Right. Left contains
-	all elements in List equal to Value. Right contains all the other
-	elements in List. The sum of lengths of Left and Right equals the
-	length of List.
+	@descr Splits @List into two groups, @Left and @Right. @Left contains
+	all elements in @List equal to @Value. @Right contains all the other
+	elements in @List. The sum of lengths of @Left and @Right equals the
+	length of @List.
+	@constrs @List must have at least one element.
 */
 split(X,    [X],    [X],[]):- !.
 split(X,    [X],    [],[X]):- !, false.
@@ -121,15 +125,16 @@ split(X, [Y|Yy], Xs,[Y|Lr]):- X \= Y, split(X, Yy, Xs,Lr).
 
 /**
 	@form split(Value, List1,List2, L1,L2, R1,R2)
-	@constrs List1 and List2 must be non-empty and of equal length.
-	@descr Splits lists List1 and List2 each into two groups. The splitting
-	is done similarly as in predicate 'split'. However, here the splitting
-	is guided by List1, that is:
-		* if i-th element of List1 is equal to Value then L1 will contain
-		i-th element of List1 and L2 will contain i-th element of List2.
-		* if i-th element of List1 is not equal to Value then R1 will
-		contain i-th element of List1 and R2 will contain i-th element
-		of List2.
+	@descr Splits lists @List1 and @List2 each into two groups. The
+	splitting is done similarly as in predicate ?split/4. However, here
+	the splitting is guided by @List1, that is:
+		* if i-th element of @List1 is equal to @Value then @L1 will
+		contain i-th element of @List1 and @L2 will contain i-th element
+		of @List2.
+		* if i-th element of @List1 is not equal to @Value then @R1 will
+		contain i-th element of @List1 and @R2 will contain i-th element
+		of @List2.
+	@constrs @List1 and @List2 must be non-empty and of equal length.
 */
 psplit(X,       [X],[Q],         [X],[Q],           [],[]):- !.
 psplit(X,       [X],[Q],           [],[],         [X],[Q]):- !, false.
@@ -157,10 +162,10 @@ how_many_([X|L], [[X, 1] | R]):- how_many_(L, R).
 
 /**
 	@form how_many(List, Counting)
-	@constrs List cannot be empty.
-	@descr Counting is a list containing lists of two elements, the
-	first of which is an element of List and the last is the number of
-	occurrences of that element in List.
+	@descr @Counting is a list containing lists of two elements, the
+	first of which is an element of @List and the last is the number of
+	occurrences of that element in @List.
+	@constrs @List cannot be empty.
 */
 how_many(L, R):- isort(L, S), how_many_(S, R).
 
@@ -168,22 +173,20 @@ how_many(L, R):- isort(L, S), how_many_(S, R).
 
 /**
 	@form dot_prod(List1, List2, P)
-	@constrs List1 and List2 cannot be empty, both must contain
+	@descr @P is the result of applying the dot product to the lists
+	@List1 and @List2 as if they were vectors.
+	@List1 = [x1, x2, ..., xn]
+	@List2 = [y1, y2, ..., yn]
+	@P = x1*y1 + x2*y2 + ... + xn*yn
+	@constrs @List1 and @List2 cannot be empty, both must contain
 	elements to which the operators '*' and '+' can be applied, and
 	must have the same length.
-	@descr P is the result of applying the dot product to the lists
-	List1 and List2 as if they were vectors.
-	List1 = [x1, x2, ..., xn]
-	List2 = [y1, y2, ..., yn]
-	P = x1*y1 + x2*y2 + ... + xn*yn
 */
 dot_prod([X], [Y], P):- P is X*Y, !.
 dot_prod([X|Xs], [Y|Ys], R):- P is X*Y, dot_prod(Xs, Ys, Q), R is P + Q, !.
 
 /**
 	@form ladder_prod(Divisor, StartValue, Coefficients, NewCoefficients, LastTerm)
-	@constrs Coefficients cannot be empty and must contain
-	elements to which the operators '*' and '+' can be applied.
 	@descr One should interpret this predicate as some sort of Ruffini's
 	rule generalisation.
 	Therefore, if Coefficients has the elements [m,n,o,p], Divisor has
@@ -204,14 +207,16 @@ dot_prod([X|Xs], [Y|Ys], R):- P is X*Y, dot_prod(Xs, Ys, Q), R is P + Q, !.
 	NewCoefficients = [T,U,V]
 
 	For the case of Ruffini's rule:
-	-> Coefficients must be a list of integer values with a '1' in the
+	-> @Coefficients must be a list of integer values with a '1' in the
 	first element.
-	-> StartValue (A) has to be 0
-	-> Divisor must divide the last element of Coefficients
-	-> NewCoefficients are the integer values representing the coefficients
+	-> @StartValue (A) has to be 0
+	-> @Divisor must divide the last element of Coefficients
+	-> @NewCoefficients are the integer values representing the coefficients
 	of a new polynomial.
-	-> LastTerm must be equal to 0 for Divisor to be a root of the
+	-> @LastTerm must be equal to 0 for Divisor to be a root of the
 	polynomial represented by Coefficients.
+	@constrs @Coefficients cannot be empty and must contain
+	elements to which the operators '*' and '+' can be applied.
 */
 ladder_prod(X, A, [Y], [], P):- P is X*(Y + A), !.
 ladder_prod(X, A, [Y|Ys], [R|L], Q):-
